23aeb7e072f9b1283d849c86c55f0c0ebf8f4145
WieraB <bielajewa.wiera@gmail.com>
2026-02-11 16:18:18 +0000

    Added Case 2.

---
+++ input_moose/case2/case2_coil_source_V2.i
@@ -0,0 +1,134 @@
+[Mesh]
+  type = MFEMMesh
+  file = coil_box_named.msh
+[]
+
+[Problem]
+  type = MFEMProblem
+[]
+
+[SubMeshes]
+  [wire]
+    type = MFEMDomainSubMesh
+    block = 'wire'
+  []
+[]
+
+[FESpaces]
+  [H1FESpace]
+    type = MFEMScalarFESpace
+    fec_type = H1
+    fec_order = FIRST
+  []
+  [HCurlFESpace]
+    type = MFEMVectorFESpace
+    fec_type = ND
+    fec_order = FIRST
+  []
+  [SubMeshH1FESpace]
+    type = MFEMScalarFESpace
+    fec_type = H1
+    fec_order = FIRST
+    submesh = wire
+  []
+  [SubMeshHCurlFESpace]
+    type = MFEMVectorFESpace
+    fec_type = ND
+    fec_order = FIRST
+    submesh = wire
+  []
+[]
+
+[Variables]
+  [electric_potential]
+    type = MFEMVariable
+    fespace = H1FESpace
+  []
+  [submesh_potential]
+    type = MFEMVariable
+    fespace = SubMeshH1FESpace
+  []
+[]
+
+[AuxVariables]
+  [current_density]
+    type = MFEMVariable
+    fespace = HCurlFESpace
+  []
+  [submesh_current_density]
+    type = MFEMVariable
+    fespace = SubMeshHCurlFESpace
+  []
+[]
+
+[AuxKernels]
+  [grad]
+    type = MFEMGradAux
+    variable = submesh_current_density
+    source = submesh_potential
+    scale_factor = -62.83185 # = -conductivity
+    execute_on = TIMESTEP_END
+  []
+[]
+
+[BCs]
+  [high_terminal]
+    type = MFEMScalarDirichletBC
+    variable = submesh_potential
+    boundary = 'CoilIn'
+    coefficient = 0.5
+  []
+  [low_terminal]
+    type = MFEMScalarDirichletBC
+    variable = submesh_potential
+    boundary = 'CoilOut'
+    coefficient = -0.5
+  []
+[]
+
+[Kernels]
+  [diff]
+    type = MFEMDiffusionKernel
+    variable = submesh_potential
+  []
+[]
+
+[Preconditioner]
+  [boomeramg]
+    type = MFEMHypreBoomerAMG
+  []
+[]
+
+[Solver]
+  type = MFEMHypreGMRES
+  preconditioner = boomeramg
+  l_tol = 1e-30
+  l_max_its = 2000
+[]
+
+[Executioner]
+  type = MFEMSteady
+  device = cpu
+[]
+
+[Transfers]
+  [submesh_transfer]
+    type = MFEMSubMeshTransfer
+    from_variable = submesh_current_density
+    to_variable = current_density
+    execution_order_group = 2
+  []
+  [submesh_potential_transfer]
+    type = MFEMSubMeshTransfer
+    from_variable = submesh_potential
+    to_variable = electric_potential
+  []
+[]
+
+[Outputs]
+  [VacuumParaViewDataCollection]
+    type = MFEMParaViewDataCollection
+    file_base = OutputData
+    vtk_format = ASCII
+  []
+[]
---
+++ scripts/error_func.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+def error(sol, sol_ref, eps, mesh, tag, save_tag):
+
+    if tag == "scalar":
+
+        abs_error = np.abs(sol - sol_ref)
+
+        abs_error_max = np.max(abs_error)
+        abs_error_mean = np.mean(abs_error)
+        
+        print("")
+        print(f"  * Max. absolute error : {abs_error_max:.3e}.")
+        print(f"  * Avg. absolute error : {abs_error_mean:.3e}.")
+        
+        mesh[f"abs_error_{save_tag}"] = abs_error
+
+
+        denom = np.abs(sol_ref)
+        num = np.abs(sol - sol_ref)
+        
+        rel_error = np.where(denom > eps, num * 100 / denom, np.nan)
+        
+        rel_error_max = np.max(rel_error[np.isfinite(rel_error)])
+        rel_error_mean = np.mean(rel_error[np.isfinite(rel_error)])
+    
+        print("")
+        print(f"  * Max. relative error : {rel_error_max:.3e} %.")
+        print(f"  * Avg. relative error : {rel_error_mean:.3e} %.")
+        
+        mesh[f"rel_error_{save_tag}"] = rel_error
+
+    elif tag == "vector":
+
+        directions = ["x", "y", "z"]
+
+        abs_error = np.zeros((len(sol), 3))
+        rel_error = np.zeros((len(sol), 3))
+
+        for i, direction in enumerate(directions):
+
+            sol_dir = sol[:, i]
+            sol_ref_dir = sol_ref[:, i]
+            
+            abs_error_dir = np.abs(sol_dir - sol_ref_dir)
+            
+            abs_error_max = np.max(abs_error_dir)
+            abs_error_mean = np.mean(abs_error_dir)
+            
+            print("")
+            print(f"  * Max. absolute error in {direction} direction  : {abs_error_max:.3e}.")
+            print(f"  * Avg. absolute error in {direction} direction  : {abs_error_mean:.3e}.")
+
+            abs_error[:, i] = abs_error_dir
+            
+            denom = np.abs(sol_ref_dir)
+            num = np.abs(sol_dir - sol_ref_dir)
+            rel_error_dir = np.where(denom > eps, num * 100 / denom, np.nan)
+            
+            rel_error_max = np.max(rel_error_dir[np.isfinite(rel_error_dir)])
+            rel_error_mean = np.mean(rel_error_dir[np.isfinite(rel_error_dir)])
+            
+            print("")
+            print(f"  * Max. relative error in {direction} direction : {rel_error_max:.3e} %.")
+            print(f"  * Avg. relative error in {direction} direction : {rel_error_mean:.3e} %.")
+
+            rel_error[:, i] = rel_error_dir
+            
+        
+        mesh[f"abs_error_{save_tag}"] = abs_error
+        mesh[f"rel_error_{save_tag}"] = rel_error
+
+    return mesh
